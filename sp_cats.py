f564c3dd7feebf5e6c965a4b572e8997097027d0
Roman <157380299+Kozdrin@users.noreply.github.com>
2024-02-13 14:12:17 +0300

    Код для вывода информации о котах

---
+++ sp_cats.py
@@ -0,0 +1,14 @@
+from cats import Cat
+
+sp1 = Cat("Барон","мальчик",2)
+sp2 = Cat("Сэм","мальчик",2)
+
+print("sp1.name=", sp1.name)
+print("sp1.sex=", sp1.sex)
+print("sp1.age=", sp1.age)
+
+print("-------")
+
+print("sp2.name=", sp2.name)
+print("sp2.sex=", sp2.sex)
+print("sp2.age=", sp2.age)
